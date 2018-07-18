02016066edf54cdfc9945390c96149a7a9ae1dab
SY <SY@192.168.2.119>
2018-07-18 13:43:30 +0900

    [fingertrip][FF] modify log msg
    modify log message
    TO-BE: logger.info("로그인화면 보여주기");

--- fingertrip/src/main/java/com/ff/finger/login/controller/loginController.java
+++ fingertrip/src/main/java/com/ff/finger/login/controller/loginController.java
@@ -11,7 +11,7 @@ public class loginController {
 	
 	@RequestMapping("/login/login.do")
 	public String index() {
-		logger.info("메인화면 보여주기");
+		logger.info("로그인화면 보여주기");
 		
 		return "login/login";
 	}
