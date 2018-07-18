44bb10a0b69549a233a9518ee60f3a6c8c010a92
SY <SY@192.168.2.119>
2018-07-18 13:07:55 +0900

    [fingerTrip][FF] upload login
    upload loginController, index.jsp, login.jsp

---
+++ fingertrip/src/main/java/com/ff/finger/login/controller/loginController.java
@@ -0,0 +1,19 @@
+package com.ff.finger.login.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class loginController {
+	private static final Logger logger = LoggerFactory.getLogger(loginController.class); 
+	
+	@RequestMapping("/login/login.do")
+	public String index() {
+		logger.info("메인화면 보여주기");
+		
+		return "login/login";
+	}
+	
+}
