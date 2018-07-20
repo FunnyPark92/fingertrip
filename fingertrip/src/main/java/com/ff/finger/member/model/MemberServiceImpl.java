6cfdc3230b0f8191df4ba1054ec5a2a465639f3f
goNi <goNi@192.168.2.128>
2018-07-20 13:29:19 +0900

    [fingertrip][FF] add Member model and mapper
    1. add Member model and mapper
    1) MemberDAO, VO, Service
    2) member.xml

---
+++ fingertrip/src/main/java/com/ff/finger/member/model/MemberServiceImpl.java
@@ -0,0 +1,12 @@
+package com.ff.finger.member.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+	@Autowired
+	private MemberDAO memberDao;
+	
+
+}
