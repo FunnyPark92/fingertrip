9052fb95d94b93e4a7b10d6489fcaafd136aba06
SY <SY@192.168.2.118>
2018-07-24 11:15:58 +0900

    [fingerTrip][FF] edit findId
    edit findId

--- fingertrip/src/main/java/com/ff/finger/member/model/MemberService.java
+++ fingertrip/src/main/java/com/ff/finger/member/model/MemberService.java
@@ -3,4 +3,5 @@ package com.ff.finger.member.model;
 public interface MemberService {
 	public int insertMember(MemberVO memberVo);
 	public int processLogin(String id, String pwd);
+	public String findId(MemberVO memberVo);
 }
