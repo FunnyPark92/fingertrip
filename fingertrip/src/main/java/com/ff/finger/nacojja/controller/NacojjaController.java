ac3b4b88a19b225949a15846ac906e886407b5ba
geoblo <geoblo@naver.com>
2018-07-26 10:11:37 +0900

    [fingertrip][FF] link and update nacojja
    link top menu and update nacojja2.jsp

--- fingertrip/src/main/java/com/ff/finger/nacojja/controller/NacojjaController.java
+++ fingertrip/src/main/java/com/ff/finger/nacojja/controller/NacojjaController.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class nacojjaController {
-	private static final Logger logger = LoggerFactory.getLogger(nacojjaController.class);
+public class NacojjaController {
+	private static final Logger logger = LoggerFactory.getLogger(NacojjaController.class);
 	
 	@RequestMapping("/nacojja/nacojjaList.do")
 	public String nacojjaList() {
@@ -16,24 +16,18 @@ public class nacojjaController {
 		return "nacojja/nacojjaList";
 	}
 	
-	
-	
-	
 	@RequestMapping("/nacojja/nacojja1.do")
 	public String nacojja1() {
-		logger.info("나코짜 목록 보여주기");
+		logger.info("나코짜 작성화면1 보여주기");
 		
 		return "nacojja/nacojja1";
 	}
 	
-	
 	@RequestMapping("/nacojja/nacojja2.do")
 	public String nacojja2() {
-		logger.info("나코짜 목록 보여주기");
+		logger.info("나코짜 작성화면2 보여주기");
 		
 		return "nacojja/nacojja2";
 	}
-	
-	
 	
 }
