ebc0086cbcd1bdb208f14ee9756c5bf0d7526d6f
SY <SY@192.168.2.117>
2018-07-30 16:31:15 +0900

    [fingerTrip][FF] upload ckEditor, heart.model
    upload ckEditor, heart.model

---
+++ fingertrip/src/main/java/com/ff/finger/heart/model/HeartVO.java
@@ -0,0 +1,32 @@
+package com.ff.finger.heart.model;
+
+public class HeartVO {
+	private int heartNo;
+	private int courseNo;
+	private int memberNo;
+
+	public int getHeartNo() {
+		return heartNo;
+	}
+	public void setHeartNo(int heartNo) {
+		this.heartNo = heartNo;
+	}
+	public int getCourseNo() {
+		return courseNo;
+	}
+	public void setCourseNo(int courseNo) {
+		this.courseNo = courseNo;
+	}
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	
+	@Override
+	public String toString() {
+		return "HeartVO [heartNo=" + heartNo + ", courseNo=" + courseNo + ", memberNo=" + memberNo + "]";
+	}
+	
+}
