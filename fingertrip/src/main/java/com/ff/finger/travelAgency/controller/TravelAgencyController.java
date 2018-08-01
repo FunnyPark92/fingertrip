dda02d6f455810127fdbd516b7bbfb45792107d2
sungB <sungB@192.168.2.104>
2018-08-01 17:27:30 +0900

    [FingerTrip][FF] update agencyWrite.jsp
    update member.js,adminTop.jsp register.jsp

---
+++ fingertrip/src/main/java/com/ff/finger/travelAgency/controller/TravelAgencyController.java
@@ -0,0 +1,36 @@
+package com.ff.finger.travelAgency.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/admin")
+public class TravelAgencyController {
+	private static final Logger logger = LoggerFactory.getLogger(TravelAgencyController.class); 
+	
+	@RequestMapping("/agency/agencyWrite.do")
+	public String agencyWrite() {
+		logger.info("여행사 회원 가입 화면");
+		
+		return "admin/agency/agencyWrite";
+	}
+	/*@RequestMapping(value="/agency/agencyWrite.do", method=RequestMethod.POST)
+	public String agencyWrite_post() {
+		logger.info("여행사 회원 가입 처리");
+		
+		return "admin/agency/agencyWrite";
+	}*/
+	
+	@RequestMapping("/agency/agencyList.do")
+	public String agencyList() {
+		logger.info("여행사 리스트");
+		
+		return "admin/agency/agencyList";
+	}
+	
+	
+	
+}
--- fingertrip/src/main/webapp/js/member.js
+++ fingertrip/src/main/webapp/js/member.js
@@ -1,5 +1,4 @@
 
-
 $(document).ready(function(){
 	
 	
@@ -43,7 +42,6 @@ function validate_username(uname){
 	var pattern = new RegExp(/^[가-힣]{2,10}$/g);
 	return pattern.test(uname);
 }
-
 function validate_userid(uid){
 	var pattern = new RegExp(/^[a-zA-Z0-9_]+$/g);
 	return pattern.test(uid);
@@ -52,13 +50,28 @@ function validate_userPwd(uPwd){
 	var pattern = new RegExp(/^[a-zA-Z]+[a-zA-Z0-9]{7,13}$/g);
 	return pattern.test(uPwd);
 }
-function validate_userHp(hp){
-	var pattern = new RegExp(/^[0-9]{3,4}$/g);
+
+
+function validate_userHp1(hp){
+	var pattern = new RegExp(/^[0-9]{3}$/g);
 	return pattern.test(hp);
 }
 function validate_userHp2(hp){
+	var pattern = new RegExp(/^[0-9]{3,4}$/g);
+	return pattern.test(hp);
+}
+function validate_userHp3(hp){
 	var pattern = new RegExp(/^[0-9]{4}$/g);
 	return pattern.test(hp);
+}
+
+function validate_license2(lc2){
+	var pattern = new RegExp(/^[0-9]{2}$/g);
+	return pattern.test(lc2);
+}
+function validate_license3(lc3){
+	var pattern = new RegExp(/^[0-9]{5}$/g);
+	return pattern.test(lc3);
 }
 
 
